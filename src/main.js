05d458fa3f88351d7632236e324912b0dc5d70d3
“postlude <useseungdeokhan@naver.com>
2020-05-17 11:58:29 +0900

    apply element-ui

--- src/main.js
+++ src/main.js
@@ -1,14 +1,62 @@
 import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import locale from 'element-ui/lib/locale/lang/ko';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+
+Vue.use(ElementUI, { locale });
 
 Vue.config.productionTip = false;
 
 new Vue({
     router,
     store,
+    methods: {
+        /**
+         * @description ElementUI alert
+         * @param {Object} arg { title, mssage, cnfirmBtnText, type, callback }
+         */
+        alert(arg) {
+            const {
+                title = '', mssage = '', cnfrmBtnTxt = '확인', ty = 'info', callback
+            } = arg;
+
+            this.$alert(mssage, title, {
+                confirmButtonText: cnfrmBtnTxt,
+                type: ty,
+                callback
+            });
+        },
+        /**
+         * @description ElementUI confirm
+         * @param {Object} arg { title, mssage, cnfrmTxt, cancelTxt, ty, excMssageTy, excMssage, cnfrmFunc }
+         */
+        async cnfrm(arg) {
+            try {
+                const {
+                    title, mssage, cnfrmTxt, cancelTxt = '취소', ty = 'warning', excMssageTy = 'success', excMssage, cnfrmFunc
+                } = arg;
+
+                await this.$confirm(mssage, title, {
+                    confirmButtonText: cnfrmTxt,
+                    cancelButtonText: cancelTxt,
+                    type: ty
+                });
+
+                await cnfrmFunc();
+
+                this.$message({ type: excMssageTy, message: excMssage });
+            } catch (e) {
+                if (e !== 'cancel') {
+                    console.error(e);
+                    this.$message({ type: 'error', message: '에러가 발생했습니다.' });
+                }
+            }
+        }
+    },
     vuetify,
     render: h => h(App)
 }).$mount('#app');
