998641afa44876b85c39a2af6f4ea087b1d37998
“postlude <useseungdeokhan@naver.com>
2020-11-01 23:03:48 +0900

    add profile page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
@@ -17,32 +16,9 @@ const routes = [
     },
     {
         path: '/',
-        name: 'home',
-        component: Home
-    },
-    {
-        path: '/about',
-        name: 'about',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-    },
-    // {
-    //     path: '/vuetify',
-    //     name: 'vuetify',
-    //     component: () => import('../views/Vuetify.vue')
-    // },
-    // {
-    //     path: '/save',
-    //     name: 'save',
-    //     component: () => import('../views/Save.vue')
-    // },
-    // {
-    //     path: '/srch',
-    //     name: 'srch',
-    //     component: () => import('../views/Srch.vue')
-    // }
+        name: 'Profile',
+        component: () => import('../views/Profile.vue')
+    }
 ];
 
 const router = new VueRouter({
---
+++ src/views/Profile.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-card class="mx-auto my-12" max-width="344">
+        <v-img
+            height="350"
+            src="@/assets/supersonic.jpg"
+        />
+
+        <v-card-title>POSTLUDE</v-card-title>
+
+        <v-card-text>
+            <div>
+                <v-icon>mdi-github</v-icon>
+                <span style="cursor: pointer" @click="moveGithub">
+                    github.com/postlude
+                </span>
+            </div>
+            <div>
+                <v-icon>mdi-post</v-icon>
+                <span style="cursor: pointer" @click="moveBlog">
+                    postlude.github.io
+                </span>
+            </div>
+            <div>
+                <v-icon>email</v-icon> seungdeokhan@naver.com
+            </div>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: 'Profile',
+    methods: {
+        moveBlog() {
+            window.open('http://postlude.github.io', '_black');
+        },
+        moveGithub() {
+            window.open('https://github.com/postlude', '_black');
+        }
+    }
+};
+</script>
