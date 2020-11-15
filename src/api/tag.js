28db796aa5c476d7e42a9c6cb3aafc8a40ef9dd2
“postlude <useseungdeokhan@naver.com>
2020-11-15 22:51:57 +0900

    add tag autocomplete

---
+++ src/api/tag.js
@@ -0,0 +1,9 @@
+import { get } from './common';
+
+/**
+ * @description 타입에 따른 모든 태그 리스트 로드
+ * @param {number} ty
+ */
+export const getTagList = ty => get({ url: '/tag/list', params: { ty } })();
+
+export const list = '';
