33e2dabc47b6424adaf41fc9abfd14d2ddc78314
postlude <seungdeokhan@naver.com>
2023-05-01 23:33:20 +0900

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,74 +1,20 @@
-import Vue from 'vue';
-import Clipboard from 'v-clipboard';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import locale from 'element-ui/lib/locale/lang/ko';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import vuetify from './plugins/vuetify';
+/**
+ * main.js
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
 
-Vue.use(ElementUI, { locale });
-Vue.use(Clipboard);
+// Components
+import App from './App.vue'
 
-Vue.config.productionTip = false;
+// Composables
+import { createApp } from 'vue'
 
-new Vue({
-    router,
-    store,
-    methods: {
-        /**
-         * @description ElementUI alert
-         * @param {Object} arg { title, mssage, cnfirmBtnText, type, callback }
-         */
-        alert(arg) {
-            const {
-                title = '', mssage = '', cnfrmBtnTxt = '확인', ty = 'info', callback
-            } = arg;
+// Plugins
+import { registerPlugins } from '@/plugins'
 
-            this.$alert(mssage, title, {
-                confirmButtonText: cnfrmBtnTxt,
-                type: ty,
-                callback
-            });
-        },
-        /**
-         * @description ElementUI confirm
-         * @param {Object} arg { title, mssage, cnfrmTxt, cancelTxt, ty, excMssageTy, excMssage, cnfrmFunc, fnlyFunc }
-         */
-        async cnfrm(arg) {
-            const {
-                title,
-                mssage,
-                cnfrmTxt,
-                cancelTxt,
-                ty = 'warning',
-                excMssageTy = 'success',
-                excMssage,
-                cnfrmFunc,
-                fnlyFunc = () => {}
-            } = arg;
+const app = createApp(App)
 
-            try {
-                await this.$confirm(mssage, title, {
-                    confirmButtonText: cnfrmTxt,
-                    cancelButtonText: cancelTxt,
-                    type: ty
-                });
+registerPlugins(app)
 
-                await cnfrmFunc();
-
-                this.$message({ type: excMssageTy, message: excMssage });
-            } catch (e) {
-                if (e !== 'cancel') {
-                    console.error(e);
-                    this.$message({ type: 'error', message: '에러가 발생했습니다.' });
-                }
-            } finally {
-                await fnlyFunc();
-            }
-        }
-    },
-    vuetify,
-    render: h => h(App)
-}).$mount('#app');
+app.mount('#app')
