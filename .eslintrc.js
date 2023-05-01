6efa3b2976757ec0cbb3c9309a37fb1c5b4d8408
postlude <seungdeokhan@naver.com>
2023-05-01 23:44:04 +0900

    default setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,15 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-  ],
-}
+	root: true,
+	env: {
+		node: true
+	},
+	extends: [
+		"plugin:vue/vue3-essential",
+		"eslint:recommended",
+		"prettier"
+		// 'plugin:prettier/recommended'
+	],
+	rules: {
+		"vue/multi-word-component-names": "off"
+	}
+};
