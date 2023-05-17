d72ea7c3482b5c42034e7f2e4a311f02bbf95d52
postlude <seungdeokhan@naver.com>
2023-05-17 23:22:41 +0900

    move home when click title

--- src/layouts/AppBar.vue
+++ src/layouts/AppBar.vue
@@ -1,7 +1,7 @@
 <template>
 	<v-app-bar flat>
 		<v-app-bar-nav-icon variant="text" @click.stop="isOpenSideBar = !isOpenSideBar" />
-		<v-toolbar-title>POSTLUDE</v-toolbar-title>
+		<v-toolbar-title style="cursor: pointer" @click="moveProfile">POSTLUDE</v-toolbar-title>
 		<v-spacer></v-spacer>
 		<v-btn variant="text" icon="mdi-magnify"></v-btn>
 	</v-app-bar>
@@ -14,50 +14,15 @@
 	</v-navigation-drawer>
 </template>
 
-<!-- <script lang="ts" setup>
-let drawer = false;
-const items = [
-	{
-		title: 'Foo',
-		value: 'foo',
-	},
-	{
-		title: 'Bar',
-		value: 'bar',
-	},
-	{
-		title: 'Fizz',
-		value: 'fizz',
-	},
-	{
-		title: 'Buzz',
-		value: 'buzz',
-	},
-];
-</script> -->
-
 <script>
 export default {
 	data: () => ({
-		isOpenSideBar: true,
-		items: [
-			{
-				title: 'Foo',
-				value: 'foo'
-			},
-			{
-				title: 'Bar',
-				value: 'bar'
-			},
-			{
-				title: 'Fizz',
-				value: 'fizz'
-			},
-			{
-				title: 'Buzz',
-				value: 'buzz'
-			}
-		]
-	})
+		isOpenSideBar: true
+	}),
+	methods: {
+		moveProfile() {
+			this.$router.push('/');
+		}
+	}
 };
 </script>
