e2fbefc1db1511ba008f7b3c17ed2bcbb423616f
postlude <seungdeokhan@naver.com>
2023-09-20 00:59:55 +0900

    fix for refresh

--- src/App.vue
+++ src/App.vue
@@ -11,4 +11,10 @@
 
 <script setup>
 import AppBar from '@/components/AppBar.vue';
+import Cookie from 'js-cookie';
+import { useStore } from 'vuex';
+
+const isSignIn = !!Cookie.get('access-token');
+const store = useStore();
+store.commit('setSignIn', isSignIn);
 </script>
