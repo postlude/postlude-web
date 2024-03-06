4ca95ee26f8238d62b71644f3ce27e866d32d42d
postlude <seungdeokhan@naver.com>
2024-03-07 00:26:00 +0900

    add api call func

--- src/util/api.js
+++ src/util/api.js
@@ -36,27 +36,11 @@ class Api {
 
 const api = new Api();
 
-/**
- * @description 개발 링크 검색
- */
+export const getAllDevLinkTags = () => api.get('/dev-links/tags');
 export const searchDevLinks = params => api.get('/dev-links', params);
-
-/**
- * @description 개발 링크 추가
- */
+export const getDevLink = id => api.get(`/dev-links/${id}`);
 export const addDevLink = devLink => api.post('/dev-links', devLink);
-
-/**
- * @description 개발 링크 삭제
- */
 export const removeDevLink = id => api.delete(`/dev-links/${id}`);
-
-/**
- * @description 개발 링크 수정
- */
 export const setDevLink = (id, devLink) => api.put(`/dev-links/${id}`, devLink);
 
-/**
- * @description 로그인
- */
 export const signIn = user => api.post('/auth/sign-in', user);
